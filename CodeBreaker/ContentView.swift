e287f3449687b94807829ce97e2e523118814647
Barbara L <lyakisheva.b@gmail.com>
2026-01-16 13:26:10 +0900

    Test Commit

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -2,23 +2,36 @@
 //  ContentView.swift
 //  CodeBreaker
 //
-//  Created by YoungSeok Shin on 1/15/26.
+//  Created by Barbara Lyakisheva on 1/15/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            pegs(colors: [.red,.green,.blue,.yellow])
+            pegs(colors: [.orange,.blue,.blue,.yellow])
+            //pegs
+            //pegs
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
-}
+    func pegs(colors: Array<Color>) -> some View{
+        HStack {
+            
+            ForEach(colors.indices, id:\.self){
+                index in
+                RoundedRectangle(cornerRadius: 10)
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundStyle(colors[index])
+            }
+            MatchMarkers(matches: [.exact, .inexact, .nomatch, .exact])
+            }
+            
+        }
+    }
+        
+    #Preview {
+        ContentView()
+    }
--- CodeBreaker/MatchMarkers.swift
+++ CodeBreaker/MatchMarkers.swift
@@ -1,14 +1,41 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by Barbara Lyakisheva on 1/16/26.
+//
+
+import SwiftUI
+
+enum Match{
+    case nomatch
+    case exact
+    case inexact
+}
+
 struct MatchMarkers: View{
+    var matches: [Match]
     var body: some View{
         HStack{
             VStack{
-                Circle()
-                Circle().strokeBorder(Color.primary, lineWidth: 3).aspectRatio(1, contentMode: .fit)
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
             }
             VStack{
-                Circle()
-                Circle().opacity(0)
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
             }
         }
     }
+    func matchMarker(peg: Int) -> some View{
+        let exactCount: Int = matches.count(where: {match in match == .exact})
+        let foundCount: Int = matches.count(where: {match in match != .nomatch})
+
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2).aspectRatio(1, contentMode: .fit)
+    }
+}
+#Preview {
+    MatchMarkers(matches:[.exact, .inexact, .nomatch])
 }
