120b292b65c1cdcbdcb529dd264a06791db93d72
Barbara L <lyakisheva.b@gmail.com>
2026-01-17 14:21:52 +0900

    Version 1 of the game. Testing how the location of the circles are located and influencing each other

--- CodeBreaker/ContentView.swift
+++ CodeBreaker/ContentView.swift
@@ -3,77 +3,41 @@
 //  CodeBreaker
 //
 //  Created by Barbara Lyakisheva on 1/15/26.
-//
+// updated: 1/16/26
 
-//Importing SwiftUI framework to use its components and views
 import SwiftUI
 
-//Defining main contentView struct that conforms to the View protocol
-//It is primary view that will be displayed to the user
 struct ContentView: View {
-    
-    //Body is a property that is required by the View protocol
-    //Since it defines what contents will be displayed on the screen
-    //Must return a View type
     var body: some View {
-        
-        //VStack arranges its child vertically
-        VStack{
-            
-            //first row of pegs: creates circles with red, green, blue and yellow
-            pegs(colors: [.red,.green,.blue,.yellow])
-            
-            //second row of pegs: creates circles with orange, blue, blue and yellow
-            pegs(colors: [.orange,.blue,.blue,.yellow])
-            
-            //Optional pegs
-            //pegs
-            //pegs
+        VStack(spacing: 35){
+            pegs(colors: [.primary,.primary,.primary], feedback: [.exact, .inexact, .inexact])
+            pegs(colors: [.primary, .primary, .primary], feedback: [.exact, .nomatch, .nomatch])
+            pegs(colors: [.primary, .primary, .primary, .primary], feedback: [.exact, .inexact, .exact, .inexact])
+            pegs(colors: [.primary, .primary, .primary, .primary], feedback: [.exact, .inexact, .exact, .nomatch])
+            pegs(colors: [.primary, .primary, .primary, .primary], feedback: [.exact, .nomatch, .inexact, .nomatch])
+            pegs(colors: [.primary, .primary, .primary, .primary, .primary, .primary], feedback: [.exact, .exact, .nomatch, .exact, .inexact, .nomatch])
+            pegs(colors: [.primary, .primary, .primary, .primary, .primary, .primary], feedback: [.exact, .exact, .inexact, .exact, .inexact, .inexact])
+            pegs(colors: [.primary, .primary, .primary, .primary, .primary], feedback: [.exact, .inexact, .inexact, .exact, .inexact])
+            pegs(colors: [.primary, .primary, .primary, .primary, .primary], feedback: [.exact, .inexact, .nomatch, .inexact, .nomatch])
         }
-        //padding adds space around the entire VStack from all edges
-        //Creates a margin between the content and the screen edges
-        .padding()
+        .padding(20)
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
-    
-    //Helper function that creates a single row of circles
-    //Parameter 'colors' is an Array of Color values that determines what color each peg should be
-    //The function returns 'some View', meaning it returns a SwiftUI view of unknown concrete type
-    func pegs(colors: Array<Color>) -> some View{
-        
-        //HStack arranges its child views horizontally
-        HStack {
-            
-            //ForEach loops through each index in the colors array
-            //'colors.indices' creates a range of valid index positions
-            //'id:\.self' tells swiftui how to uniquely identify each element though index itself
+
+    func pegs(colors: Array<Color>, feedback: Array<Match>) -> some View{
+        HStack(spacing: 10) {
             ForEach(colors.indices, id:\.self){
                 index in
-                
-                //Rounded rectangle creates a rectangle shape with rounded corners
-                RoundedRectangle(cornerRadius: 10)
-                
-                //aspectRatio - makes the shape of a perfect square
-                //1 - width-to-height ratio of 1:1 (square)
-                //contentMode - ensures the shape fits within available space
-                    .aspectRatio(1, contentMode: .fit)
-                
-                //foregroundStyle  - colors the spae with the color from the array at the current index
-                //example: first shape gets colors[0].red and etc.
+                Circle()
+                    .frame(width: 45, height: 45)
+                    //.aspectRatio(contentMode: .fit)
                     .foregroundStyle(colors[index])
             }
-            
-            
-            //MatchMaker -  custom component that defines feedback indicators
-            //Shows which guesses were: exact(correct color, correct position), inexact(correct color, but wrong position) and nomatch(wrong color, wrong position)
-            //in this case 'matches' contains 4 feedback values that correspond to the 4 pegs above
-            MatchMarkers(matches: [.exact, .inexact, .nomatch, .exact])
+            MatchMarkers(matches: feedback)
             }
-            
         }
     }
-        
 
-//preview is a macro that allows to see how the contentview lookss in xcode's preview canvas w/o the need to run the full app
     #Preview {
         ContentView()
     }
